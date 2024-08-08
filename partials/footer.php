f2cb0a72563651d9798be0fa04c73596bfd7a032
JVSecundo <joaovictorsecundo402@gmail.com>
2024-08-08 17:04:49 -0300

    update

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,20 @@
 <br>
+
 <footer class="bg-dark text-white text-center py-3">
-    <p class="mb-0">&copy; 2024 Posto de Vendas do Campus Ceres</p>
-</footer>
+
+	<p>&copy; 2024 Posto de Vendas do Campus Ceres</p>
+	<p>Entre em contato pelo e-mail:</p>
+    <p>joao.secundo@estudante.ifgoiano.edu.br</p>
+    <p>isaque.romualdo@estudante.ifgoiano.edu.br</p>
+    <p>gabriel.brito1@estudante.ifgoiano.edu.br</p>
+
+        <ul>
+            <li><a href="#">Política de privacidade</a></li>
+            <li><a href="#">Termos de uso</a></li>
+            <li><a href="#">Sobre nós</a></li>
+            <li><a href="#">Contato</a></li>
+        </ul>
+    </footer>
 
 <!-- JavaScript -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -9,3 +22,4 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
+
